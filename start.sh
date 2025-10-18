0474c9a030720717feb657b3d154fb72b4d8198e
Nusrat Rahman <rahmannusrat890@gmail.com>
2025-10-18 22:11:50 +0600

    Update start.sh with final changes

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 
+# Start monitor.py in the background
 python3 monitor.py &
 
-# Start Flask on Render port
-flask run --host=0.0.0.0 --port=$PORT
-
+# Start Flask on all interfaces
+flask run --host=0.0.0.0 --port=5000
